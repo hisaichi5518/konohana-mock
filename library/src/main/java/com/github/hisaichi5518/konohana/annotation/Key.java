854ffe843b802123359057eca042a6f04a4760dc
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-14 23:31:05 +0900

    Add konohana annotations

---
+++ library/src/main/java/com/github/hisaichi5518/konohana/annotation/Key.java
@@ -0,0 +1,8 @@
+package com.github.hisaichi5518.konohana.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+public @interface Key {
+}
