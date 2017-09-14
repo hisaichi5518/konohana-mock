19f07aa31bbc7021631154a2e5c640e9b64ce04d
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-15 04:27:41 +0900

    Remove library annotation

--- library/src/main/java/com/github/hisaichi5518/konohana/annotation/Table.java
+++
@@ -1,8 +0,0 @@
-package com.github.hisaichi5518.konohana.annotation;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
-
-@Target(ElementType.TYPE)
-public @interface Table {
-}
