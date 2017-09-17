b8ab33377a1b05f3a93a88b9bb938a10f9a2217a
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-17 23:21:58 +0900

    Add LongTypeAdapterTest

---
+++ library/src/test/java/com/github/hisaichi5518/konohana/typeadapter/LongTypeAdapterTest.java
@@ -0,0 +1,48 @@
+package com.github.hisaichi5518.konohana.typeadapter;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.github.hisaichi5518.konohana.BuildConfig;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class LongTypeAdapterTest {
+
+    private LongTypeAdapter adapter;
+    private SharedPreferences prefs;
+
+    @Before
+    public void setup() {
+        adapter = new LongTypeAdapter();
+        prefs = RuntimeEnvironment.application.getSharedPreferences("tests", Context.MODE_PRIVATE);
+    }
+
+    @Test
+    public void get_WithDefault() throws Exception {
+        assertThat(adapter.get(prefs, "key", 0L)).isEqualTo(0L);
+
+        adapter.set(prefs, "key", 1L);
+
+        assertThat(adapter.get(prefs, "key", 0L)).isEqualTo(1L);
+    }
+
+    @Test
+    public void get_WithoutDefault() throws Exception {
+        assertThat(adapter.get(prefs, "key")).isEqualTo(0L);
+
+        adapter.set(prefs, "key", 1L);
+
+        assertThat(adapter.get(prefs, "key")).isEqualTo(1L);
+    }
+
+}
