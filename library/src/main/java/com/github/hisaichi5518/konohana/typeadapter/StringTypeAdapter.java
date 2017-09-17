2d688b10506ac4655a7e05dd429f39963c7537a7
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-17 23:28:37 +0900

    return null

--- library/src/main/java/com/github/hisaichi5518/konohana/typeadapter/StringTypeAdapter.java
+++ library/src/main/java/com/github/hisaichi5518/konohana/typeadapter/StringTypeAdapter.java
@@ -7,7 +7,7 @@ import com.github.hisaichi5518.konohana.PreferenceTypeAdapter;
 public class StringTypeAdapter implements PreferenceTypeAdapter<String> {
     @Override
     public String get(SharedPreferences prefs, String key) {
-        return get(prefs, key, "");
+        return get(prefs, key, null);
     }
 
     @Override
